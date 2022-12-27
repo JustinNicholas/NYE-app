763617887c9f3de989df6cae810cab019e2d9cc4
Justin Nicholas <nicholasj964@yahoo.com>
2022-12-27 12:46:42 -0600

    new year time in milliseconds added and exact date added

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import moment from "moment/moment";
+import DateCountdown from 'react-date-countdown-timer';
 
 function Countdown(){
 
@@ -8,12 +9,19 @@ function Countdown(){
     const current = new Date();
     console.log(current);
     const currentTime = moment(current).format('MMMM Do YYYY, h:mm:ss a');
+    console.log('current time in mill', current.getTime());
     // const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()} ${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
+    const newYear = 1672552800000;
+    const newYearTime = moment(newYear).format('MMMM Do YYYY, h:mm:ss a');
+
+    console.log(newYear);
+
 
     return(
         <div>
             <h1>This will be the coutdown</h1>
             <h2>{currentTime}</h2>
+            <h3>{newYearTime}</h3>
         </div>
     )
 }
