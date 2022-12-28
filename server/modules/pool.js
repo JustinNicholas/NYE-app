903ef6dad9035968ad8cb28d0ec4742b03a98f47
Justin Nicholas <nicholasj964@yahoo.com>
2022-12-28 11:09:42 -0600

    get server up and running

---
+++ server/modules/pool.js
@@ -0,0 +1,18 @@
+const pg = require('pg')
+const Pool = pg.Pool;
+const config = {
+    database: 'nye_resolutions', // the name of the database
+    host: 'localhost', // where is your database
+    port: 5432, // the port number for your database, 5432 is the default
+    max: 10, // how many connections at one time
+    idleTimeoutMillis: 30000 // 30 seconds to try to connect
+};
+
+const pool = new Pool(config);
+
+pool.on('connect', (client) => {
+    console.log('Unexpected error on idle pg client', err);
+    process.exit(-1);
+});
+
+module.exports = pool;
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const resolution = require('./routes/resolution.router.js');
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static('build'));
+
+// Express Routes
+
+app.use('/resolution', resolution);
+
+// start server
+
+app.listen( PORT, () => {
+    console.log('Listening on port ', PORT);
+});
